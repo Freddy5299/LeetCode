073de7e501ac6ee46341e4b1c45a278064ace171
Freddy5299 <1172599631@qq.com>
2020-05-13 18:42:55 +0800

    new file:   "\344\270\244\344\270\252\346\225\260\347\273\204\347\232\204\344\270\255\344\275\215\346\225\260\344\271\213\345\222\214.py" 	new file:   "\346\234\200\351\225\277\345\233\236\346\226\207.py"

---
+++ 两个数组的中位数之和.py
@@ -0,0 +1,43 @@
+from heapq import *
+
+
+class DoubleHeap(object):
+    def __init__(self):
+        self.maxh = []
+        self.minh = []
+        heapify(self.maxh)
+        heapify(self.minh)
+
+    def insert(self, val):
+        heappush(self.minh, val)
+        heappush(self.maxh, -heappop(self.minh))
+        if len(self.maxh) > len(self.minh):
+            heappush(self.minh, -heappop(self.maxh))
+
+    def findMedian(self):
+        if len(self.maxh) == len(self.minh):
+            return (self.minh[0] - self.maxh[0]) / 2.0
+        return self.minh[0] / 1.0
+
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        dh = DoubleHeap()
+        for num in nums1:
+            dh.insert(num)
+
+        for num in nums2:
+            dh.insert(num)
+
+        return dh.findMedian()
+
+if __name__ == '__main__':
+    s = Solution()
+    nums1 = [1, 2,9,2,3,4,6,1,4,4,5,6,100,100,100,100,1000,1000,1000]
+    nums2 = [3, 4,5,5,100,100,100,100,100,100,100,100,100,1000,1000,1000]
+    print(s.findMedianSortedArrays(nums1=nums1,nums2=nums2))
